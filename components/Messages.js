d84a719b478c469e8fc1154157ef28754e3a1e2f
greko <gyan2020cs093@abesit.edu.in>
2021-12-21 01:13:47 +0530

    Update Messages.js

--- components/Messages.js
+++ components/Messages.js
@@ -1,6 +1,22 @@
+import { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery} from "react-moralis";
+// import SendMessage from "./SendMessages";
+
+// Only show messages from the last 15  minutes
+const MINS_DURATION = 15;
 function Messages() {
     const { user } =useMoralis();
+    // const endOfMessagesRef = userRef(null);
+    const { data ,loading, error } =useMoralisQuery(
+        'Messages',
+        (query) => query.ascending('createdAt').greaterThan
+        ('createdAt', new Date(Date.now() - 1000 * 60 * MINS_DURATION))
+        ,
+        [],
+        {
+            live: true,
+        }
+    );
 
     return (
         <div className="pb-56">
