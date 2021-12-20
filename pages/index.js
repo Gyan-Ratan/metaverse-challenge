87f71c13371beb26493f78126236a45e314b8b5a
greko <gyan2020cs093@abesit.edu.in>
2021-12-21 00:58:17 +0530

    Added Messages.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Login from '../components/Login'
 import { useMoralis } from "react-moralis";
 import Header from '../components/Header';
+import Messages from '../components/Messages';
 
 export default function Home() {
   const {isAuthenticated,logout} = useMoralis();
@@ -13,7 +14,11 @@ export default function Home() {
         <title>metaverse challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <div className="max-w-screen-2xl mx-auto">
       <Header/>
+      <Messages />
+      </div>
       <button onClick={logout}>logout</button>
     </div>
   )
